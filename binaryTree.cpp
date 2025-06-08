09c5f0d110f81627fdbe2f7fd03d7dee46e362d8
rajguptaaa <kumaaraj11@gmail.com>
2025-06-08 09:46:46 +0530

    summer

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class Node{
-    public:
+public:
     int data;
     Node* left;
     Node* right;
@@ -42,16 +43,36 @@ void inOrder(Node* root){
     inOrder(root->right);
     
 }
-int main(){
 
+void levelOrder(Node* root){
+    if(!root){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* curr = q.front();
+        q.pop();
+        cout << curr->data << " ";
+        if(curr->left) q.push(curr->left);
+        if(curr->right) q.push(curr->right);
+    }
+}
+int main(){
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
 
     preOrder(root);
     cout<<endl;
     postOrder(root);
     cout<<endl;
     inOrder(root);
+    cout<<endl;
+    levelOrder(root);
+    
     return 0;
 }
